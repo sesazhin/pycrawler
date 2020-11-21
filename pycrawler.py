ed4a2eacd2209c14a41841fcfae797fbeb18745e
sesazhin <sesazhin@cisco.com>
2020-11-21 12:59:26 +0300

    added logger

--- pycrawler.py
+++ pycrawler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import logging
+import logging.handlers
 import time
 
 from genie.conf import Genie
@@ -12,6 +13,58 @@ from unicon.core import errors
 
 from pathlib import Path
 
+
+def set_main_logging(logging_level_console='ERROR', logging_level_file='INFO') -> logging.getLogger():
+    """
+    Sets logger (for stderr, file).
+
+    In case logging directory doesn't exist, will try to create it.
+    If it's not possible to save to logging directory, will start to save to the directory with the script.
+
+    :param DIRECTORY: Logging directory
+    :param LOG_NAME: Logging file name
+    :param LOG_FILE_SIZE: Size of each logging file in MBytes
+    :return: link to the root logger object.
+    """
+
+    logging_level_to_num = {'CRITICAL': 50, 'ERROR': 40, 'WARNING': 30, 'INFO': 20, 'DEBUG': 10}
+
+    logging_num_console = logging_level_to_num[logging_level_console]
+    logging_num_file = logging_level_to_num[logging_level_file]
+
+    global root_logger
+    root_logger = logging.getLogger('main_logger')
+    root_logger.propagate = False
+    root_logger.setLevel(logging.DEBUG)
+    logFormatter = logging.Formatter("%(asctime)s - %(filename)s - line %(lineno)s - %(funcName)s - %(levelname)s: %(message)s")
+
+    # Initialize logging to console:
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(logging_num_console)
+    consoleHandler.setFormatter(logFormatter)
+    root_logger.addHandler(consoleHandler)
+
+    log_dir_name = 'log'
+    par_dir_path = Path(__file__).resolve().parents[1]
+
+    abs_log_path = path.join(par_dir_path, log_dir_name)
+    create_non_existing_dir(abs_log_path)
+
+    LOGFILE = path.join(abs_log_path, 'pycrawler.log')
+    LOG_FILE_SIZE = 20
+
+    try:
+        file_handler = logging.handlers.RotatingFileHandler(LOGFILE, maxBytes=(1048576 * LOG_FILE_SIZE), backupCount=10,
+                                                            encoding='utf-8')   # save logging output to file up to 20 Mbytes size,
+                                                                                # keep 10 files in logging directory
+        file_handler.setLevel(logging_num_file)
+        file_handler.setFormatter(logFormatter)
+        root_logger.addHandler(file_handler)
+
+    except PermissionError:
+        root_logger.exception(f'Unable to create log file: {LOGFILE}.\nLogs not saved!')
+
+    return root_logger
 
 def create_non_existing_dir(dir_path):
     if not path.exists(dir_path):
@@ -83,10 +136,17 @@ def collect_device_commands(testbed, commands_to_gather, dir_name):
 
 def main():
     global log
+    log = sup.set_main_logging(logging_level_console, logging_level_file)
+
+    '''
     format = '%(asctime)s - %(filename)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=format)
+    ogging.basicConfig(level=logging.INFO, format=format)
+
+    logging_level_console = 'INFO'
+    logging_level_file = 'INFO'
 
     log = logging.getLogger(__name__)
+    '''
 
     script_directory = Path(__file__).resolve().parents[0]
 
