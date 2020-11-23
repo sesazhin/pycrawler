4837d93db842ce72dfec5e44f999723cd83b3603
sesazhin <sesazhin@cisco.com>
2020-11-23 13:06:00 +0300

    added gather_delta_commands function

--- pycrawler_lib/main.py
+++ pycrawler_lib/main.py
@@ -42,12 +42,12 @@ def get_gzip_files(current_filename: str, filename_to_compare: str) -> str:
         return ''
 
 
-def remove_file(oldest_file) -> None:
+def remove_file(filename) -> None:
     try:
-        remove(oldest_file)
-        log.info(f'oldest_file has been removed successfully: {oldest_file}')
+        remove(filename)
+        log.info(f'File has been removed successfully: {filename}')
     except PermissionError as e:
-        log.error(f'Unable to delete gzip file: {oldest_file}.'
+        log.error(f'Unable to delete file: {filename}.'
                   f'Insufficient privileges. Error: {e}')
 
 
@@ -178,6 +178,106 @@ def collect_device_commands(testbed, commands_to_gather: Dict,
             continue
 
 
+def collect_delta_device_commands(testbed, commands_to_gather: Dict,
+                            dir_name: str, file_size_to_gzip: int, num_to_store=10) -> None:
+    abs_dir_path = join(dirname(__file__), dir_name)
+
+    sup.create_non_existing_dir(abs_dir_path)
+
+    log.debug('Starting to collect output of the commands')
+
+    for device_name, device in testbed.devices.items():
+        # get operating system of a device from pyats_testbed.yaml
+        device_os = device.os
+        device_path = join(abs_dir_path, device_name)
+        sup.create_non_existing_dir(device_path)
+        device_path_delta = join(adevice_path, 'delta')
+        sup.create_non_existing_dir(device_path_delta)
+
+        try:
+            device.connect(log_stdout=False)
+        except errors.ConnectionError:
+            log.error(f'Failed to establish connection to: {device.name}.'
+                      f'Check connectivity and try again.')
+            continue
+
+        time_now_readable = time.strftime('%d %b %Y %H:%M:%S', time.localtime())
+        current_timestamp = int(time.time())
+
+        log.info(f'time_now: {time_now_readable}')
+
+        if commands_to_gather.get(device_os):
+            flag_delta_filename = join(Path(__file__).resolve().parents[0], 'clear_flag.tmp')
+
+            if exists(flag_delta_filename):
+                # counters have been cleared already
+                log.info(f'flag_delta_filename {flag_delta_filename} exists')
+
+                for command in commands_to_gather[device_os]:
+                    filename_command = command.replace(' ', '_')
+                    filename_command = filename_command.replace('*', 'all')
+                    filename = device_name + '_' + filename_command
+                    abs_filename = join(device_path_delta, filename)
+                    log.info(f'filename: {abs_filename}')
+
+                    command_output = device.execute(command[0], log_stdout=True)
+
+                    # remove_file(filename)
+
+                    # fixing cosmetic bug with '>' on the last line of FTD's output
+                    if device_os == 'fxos' and command_output[-1:] == '>':
+                        command_output = '\n'.join(command_output.split('\n')[:-1]) + '\n'
+
+                    try:
+                        with open(flag_delta_filename, mode='r') as fp:
+                            fp.read(clear_timestamp)
+                    except PermissionError as e:
+                        log.error(f'Unable to read delta file: {flag_delta_filename}.'
+                                  f'Insufficient privileges. Error: {e}')
+                        exit(1)
+
+                    clear_timestamp = int(clear_timestamp)
+                    clear_time_readable = datetime.datetime.fromtimestamp(clear_timestamp)
+                    clear_time_readable = clear_time_readable.strftime('%d %b %Y %H:%M:%S')
+
+                    seconds_interval = current_timestamp - clear_timestamp
+
+                    delta_time_string = f'Delta output for the interval: {clear_time_readable} - {time_now_readable}.' \
+                                        f' Interval in seconds: {seconds_interval}'
+                    write_commands_to_file(abs_filename, command_output, delta_time_string)
+
+                # get all big non-gz files (in plain text) for this device
+                only_big_files = get_files_to_gz(device_path_delta, file_size_to_gzip)
+
+                if len(only_big_files) > 0:
+                    archive_dir_name = 'archive'
+                    abs_archive_path = join(device_path_delta, archive_dir_name)
+                    sup.create_non_existing_dir(abs_archive_path)
+
+                    # gz all big plain text files for this device
+                    gz_files(only_big_files, abs_archive_path)
+                    # remove the oldest gz file for each command for this device
+                    remove_old_gz_files(only_big_files, abs_archive_path, num_to_store)
+
+            else:
+                # counters haven't been cleared already
+                log.info(f'flag_delta_filename {flag_delta_filename} does not exist')
+
+            # Block of run clear commands and update tmp file with new timestamp:
+            for command in commands_to_gather[device_os]:
+                command_output = device.execute(command[1], log_stdout=True)
+                log.info(f'Run command: {command[1]}. Command_output: {command_output}')
+
+            try:
+                with open(flag_delta_filename, mode='w') as fp:
+                    fp.write(current_timestamp)
+            except PermissionError as e:
+                log.error(f'Unable to create delta file: {flag_delta_filename}.'
+                          f'Insufficient privileges. Error: {e}')
+                exit(1)
+            # End of run clear commands and update tmp file with new timestamp
+
+
 def main():
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s')
@@ -221,7 +321,8 @@ def main():
 
     dir_name = join(script_directory, 'gathered_commands')
 
-    collect_device_commands(testbed, commands_to_gather, dir_name, file_size_to_gzip, num_to_store)
+    # collect_device_commands(testbed, commands_to_gather, dir_name, file_size_to_gzip, num_to_store)
+    collect_delta_device_commands(testbed, delta_commands_to_gather, dir_name, file_size_to_gzip, num_to_store)
 
 
 if __name__ == '__main__':
